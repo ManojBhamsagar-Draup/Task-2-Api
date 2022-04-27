262b1711d3a4f8a74a237647e3240ebf2dfa9acc
manoj sagar <manojsagar066@gmail.com>
2022-04-27 15:54:27 +0530

    assigned roles and access

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,12 +1,14 @@
 from Models.model import Users
 from flask import Response, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
 from controllers.role_decorator import roles_required
 from flask_restful import Resource
 from Models.UserErrors import *
 
 
 class UserApi(Resource):
+    @jwt_required()
+    @roles_required(['admin'])
     def get(self, email):
         try:
             user = Users.objects(email=email).to_json()
@@ -26,6 +28,8 @@ class UsersApi(Resource):
         users = Users.objects.to_json()
         return Response(users, mimetype='application/json', status=200)
 
+    @jwt_required()
+    @roles_required(['admin'])
     def delete(self):
         body = request.get_json()
         try:
@@ -40,12 +44,14 @@ class UsersApi(Resource):
 
 
 class UserDataApi(Resource):
-    # @jwt_required()
+    @jwt_required()
+    @roles_required(['admin'])
     def get(self, email):
         user = Users.objects(email=email).to_json()
         print(user)
         return Response(user, mimetype='application/json', status=200)
 
+    @jwt_required()
     def put(self, email):
         user = Users.objects(email=email).to_json()
         if user == "[]":
@@ -57,5 +63,3 @@ class UserDataApi(Resource):
         print(total_calories)
         Users.objects(email=email).update(__raw__={'$push': {'calories': total_calories}})
         return Response("data added", mimetype='application/json', status=200)
-
-
