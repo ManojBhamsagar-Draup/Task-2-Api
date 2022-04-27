556456f53562ca94966d396fd803d8181301399b
manoj sagar <manojsagar066@gmail.com>
2022-04-27 12:23:27 +0530

    added user/admin apis

--- controllers/controller.py
+++ controllers/controller.py
@@ -23,12 +23,36 @@ class UsersApi(Resource):
         users = Users.objects.to_json()
         return Response(users, mimetype='application/json', status=200)
 
+    def delete(self):
+        body = request.get_json()
+        try:
+            user = Users.objects(email=body['email']).to_json()
+            if user == "[]":
+                raise UserNotFoundError("the user doesn't exist")
+            Users.objects(email=body['email']).delete()
+            return Response('user deleted', mimetype='application/json', status=200)
+        except UserNotFoundError:
+            res = body['email'] + " does not exist"
+            return Response(res, mimetype='application/json', status=404)
+
 
 class UserDataApi(Resource):
-    @jwt_required()
+    # @jwt_required()
     def get(self, email):
         user = Users.objects(email=email).to_json()
         print(user)
         return Response(user, mimetype='application/json', status=200)
+
+    def put(self, email):
+        user = Users.objects(email=email).to_json()
+        if user == "[]":
+            res = email + " does not exists"
+            return Response(res, mimetype='application/json', status=404)
+
+        body = request.get_json()
+        total_calories = body['morning'] + body['afternoon'] + body['night'] + body['extra']
+        print(total_calories)
+        Users.objects(email=email).update(__raw__={'$push': {'calories': total_calories}})
+        return Response("data added", mimetype='application/json', status=200)
 
 
